9fa2fc010bf9e3eeaa08e71fecb608efd103bb6b
giulia-ciampa <giulia.ciampa7@gmail.com>
2026-07-23 17:17:03 +0200

    registrazione

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router"
 import Bw5Navbar from "./components/Bw5Navbar"
 import "bootstrap/dist/css/bootstrap.min.css"
+import Registrazione from "./components/Registrazione"
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Bw5Navbar></Bw5Navbar>
 
         <Routes>
-          <Route path="/" />
+          <Route path="/registrazione" element={<Registrazione />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Registrazione.tsx
+++ src/components/Registrazione.tsx
@@ -1,5 +1,134 @@
+import { useState } from "react"
+import { Button, Col, Container, Row } from "react-bootstrap"
+import Form from "react-bootstrap/Form"
+import { useNavigate } from "react-router"
+
 function Registrazione() {
-  return <></>
+  const navigate = useNavigate()
+
+  // 1. Stato per raccogliere i dati del form
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    nome: "",
+    cognome: "",
+  })
+
+  // Aggiorna lo stato man mano che l'utente scrive nei campi
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  // 2. Funzione chiamata all'invio del Form
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault() // Evita il ricaricamento della pagina
+
+    try {
+      const response = await fetch("http://localhost:3001/auth/registrazione", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+
+      if (response.ok) {
+        alert("Registrazione avvenuta con successo!")
+        //reindirizza alla pagina di login
+        navigate("/login")
+      } else {
+        alert("Errore durante la registrazione")
+      }
+    } catch (error) {
+      console.error("Errore di rete:", error)
+      alert("Impossibile connettersi al server")
+    }
+  }
+
+  return (
+    <Container className="w-50 d-flex flex-column align-content-center vh-100 justify-content-center">
+      <Row>
+        <Col>
+          <Form
+            onSubmit={handleSubmit}
+            className="my-5 border border-2 border-primary rounded-3 p-3"
+          >
+            <Form.Group className="mb-4" controlId="usernameInput">
+              <Form.Label className="w-100 text-center">username</Form.Label>
+              <Form.Control
+                className="w-100 text-center"
+                type="text"
+                placeholder="Digita qui il tuo username..."
+                value={formData.username}
+                onChange={handleChange}
+                name="username"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="emailInput">
+              <Form.Label className="w-100 text-center">email</Form.Label>
+              <Form.Control
+                className="w-100 text-center"
+                type="email"
+                placeholder="Digita qui la tua email..."
+                autoComplete="off"
+                value={formData.email}
+                onChange={handleChange}
+                name="email"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="passwordInput">
+              <Form.Label className="w-100 text-center">password</Form.Label>
+              <Form.Control
+                className="w-100 text-center"
+                type="password"
+                placeholder="Digita qui la tua password..."
+                autoComplete="new-password"
+                value={formData.password}
+                onChange={handleChange}
+                name="password"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="nomeInput">
+              <Form.Label className="w-100 text-center">nome</Form.Label>
+              <Form.Control
+                className="w-100 text-center"
+                type="text"
+                placeholder="Digita qui il tuo nome..."
+                value={formData.nome}
+                onChange={handleChange}
+                name="nome"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="cognomeInput">
+              <Form.Label className="w-100 text-center">cognome</Form.Label>
+              <Form.Control
+                className="w-100 text-center"
+                type="text"
+                placeholder="Digita qui il tuo cognome..."
+                value={formData.cognome}
+                onChange={handleChange}
+                name="cognome"
+              />
+            </Form.Group>
+
+            <div className="d-flex justify-content-center mt-4">
+              <Button type="submit" variant="primary" className="">
+                Registrati
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Registrazione
