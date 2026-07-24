e4651c0852762625d102840640d629f1a8d96af4
giulia-ciampa <giulia.ciampa7@gmail.com>
2026-07-24 15:37:12 +0200

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -57,7 +57,10 @@ function Home() {
           </div>
         </Col>
         <Col md={4} style={{ cursor: "pointer" }}>
-          <div className="p-4 rounded shadow bg-dark bg-opacity-75 text-white h-100">
+          <div
+            className="p-4 rounded shadow bg-dark bg-opacity-75 text-white h-100"
+            onClick={() => navigate("/fatture")}
+          >
             <div
               className="mb-3"
               style={{ color: "#ffc107", fontSize: "3.5rem" }}
